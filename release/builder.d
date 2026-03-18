dc3073a3cb2d9d5325fcff446a90b90c07de7985
thespilindell <sksdim149@gmail.com>
2026-03-18 20:42:01 +0400

    Add files via upload

---
+++ release/builder.d
@@ -0,0 +1,1 @@
+/home/thespelendell/builder/target/release/builder: /home/thespelendell/builder/src/main.rs
---
+++ release/deps/builder-bb608c56a7dde6a6.d
@@ -0,0 +1,5 @@
+/home/thespelendell/builder/target/release/deps/builder-bb608c56a7dde6a6.d: src/main.rs
+
+/home/thespelendell/builder/target/release/deps/builder-bb608c56a7dde6a6: src/main.rs
+
+src/main.rs:
